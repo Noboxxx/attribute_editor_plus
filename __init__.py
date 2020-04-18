1107f7e1c99b1695002a89e71fda4dbacaad1e5c
Pint <pierre-laurent@outlook.com>
2020-04-18 22:07:13 +0200

    much stuff

--- __init__.py
+++ __init__.py
@@ -1,10 +1,13 @@
 from PySide2.QtWidgets import *
-from PySide2.QtGui import *
 from shiboken2 import wrapInstance
 import maya.OpenMayaUI as omui
+import pymel.core as pm
 from maya import cmds
-import copy
 
+
+class Attribute(object):
+
+    def __init__(self, name):
 
 def f_attr(node, attr):
     return '{0}.{1}'.format(node, attr)
@@ -15,11 +18,39 @@ def get_widget(object_name, type_):
     return wrapInstance(long(pointer), type_)
 
 
+# def get_user_attrs(node):
+#     attributes = list()
+#     for item in pm.listAttr(node, userDefined=True):
+#         if isinstance(item, pm.Attribute):
+#             attr = item
+#         else:
+#             try:
+#                 attr = pm.Attribute(f_attr(node, item))
+#             except:
+#                 continue
+#
+#         attributes.append(attr)
+#     return attributes
+
+def get_user_attrs(node):
+    attributes = list()
+    for item in cmds.listAttr(node.name(), userDefined=True):
+        attributes.append(item)
+    return attributes
+
+
+def get_intersection(ls):
+    test = set()
+    for a, i in enumerate(ls):
+        if a == 0:
+            test.update(i)
+            continue
+        test.intersection_update(i)
+    return list(test)
+
+
 class AttributeEditorPlus(QDialog):
-    transform_attrs = ['translateX', 'translateY', 'translateZ',
-                       'rotateX', 'rotateY', 'rotateZ',
-                       'scaleX', 'scaleY', 'scaleZ',
-                       'shearXY', 'shearXZ', 'shearYZ',
+    transform_attrs = ['translate', 'rotate', 'scale', 'shear',
                        'rotateOrder', 'rotateAxis',
                        'inheritsTransform']
 
@@ -60,58 +91,107 @@ class AttributeEditorPlus(QDialog):
         return dialog
 
     def refresh(self):
-        selection = cmds.ls(sl=True) or list()
+        selection = pm.selected()
+        common_attributes = get_intersection([get_user_attrs(node) for node in selection])
 
-        if not selection:
-            return
+        for attr in common_attributes:
+            print attr
 
-        self.node_name.setText(selection[-1])
-        self.node_type.setText(cmds.nodeType(selection[-1]))
+        # for node in selection:
+        #     self.node_name.setText(node.name())
+        #     self.node_type.setText(node.type())
+        #
+        #     user_defined_attrs = list()
+        #
+        #     for item in pm.listAttr(node, userDefined=True):
+        #         if isinstance(item, pm.Attribute):
+        #             attr = item
+        #         else:
+        #             try:
+        #                 attr = pm.Attribute(f_attr(node, item))
+        #             except:
+        #                 continue
+        #
+        #         user_defined_attrs.append(attr)
+        #
+        #     attributes = dict()
+        #     # for group, attrs in self.groups.items():
+        #     #     attributes[group] = list()
+        #     #     for attr in attrs:
+        #     #         attributes[group].append(pm.PyNode(f_attr(node, attr)))
+        #
+        #     attributes['userDefined'] = user_defined_attrs
+        #
+        # for title, attrs in commun_attributes.items():
+        #     widget = self.create_group_widget(title, attrs)
+        #     self.attrs_lay.addWidget(widget)
 
-        user_defined_attrs = cmds.listAttr(selection[-1], userDefined=True)
+    def create_group_widget(self, title, attrs):
+        group_box_lay = QVBoxLayout()
 
-        attributes = copy.deepcopy(self.groups)
-        attributes['userDefined'] = user_defined_attrs
+        group_box = QGroupBox()
+        group_box.setTitle(title)
+        group_box.setLayout(group_box_lay)
 
-        for group, attributes in attributes.items():
-            group_lay = QVBoxLayout()
+        for attr in attrs:
+            attr_lay = self.create_attr_lay(attr)
+            if attr_lay is None:
+                continue
+            group_box_lay.addLayout(attr_lay)
 
-            group_box = QGroupBox()
-            group_box.setTitle(group)
-            group_box.setLayout(group_lay)
-            self.attrs_lay.addWidget(group_box)
+        return group_box
 
-            for attr in attributes:
-                full_attr = f_attr(selection[-1], attr)
-                if not cmds.objExists(full_attr):
-                    print attr
-                    continue
+    def create_attr_lay(self, attr):
+        children = list()
 
-                value = cmds.getAttr(full_attr)
-                locked = cmds.getAttr(full_attr, lock=True)
-                type_ = cmds.getAttr(full_attr, type=True)
+        try:
+            children = attr.children()
+        except:
+            pass
 
-                attr_name = QLineEdit()
-                attr_name.setText(attr)
-                attr_name.setEnabled(not locked)
+        name = attr.name(includeNode=False)
+        locked = attr.isLocked()
 
-                attr_type = QLineEdit()
-                attr_type.setText(type_)
-                attr_type.setEnabled(not locked)
+        name_line = QLineEdit()
+        name_line.setText(name)
 
-                attr_value = self.create_value_widget(type_, value)
-                attr_value.setEnabled(not locked)
+        type_line = QLineEdit()
+        type_line.setText(attr.type())
 
-                attr_lay = QHBoxLayout()
-                attr_lay.addWidget(attr_name)
-                attr_lay.addWidget(attr_type)
-                attr_lay.addWidget(attr_value)
+        value_lay = self.create_value_layout(attr, lock=locked)
 
-                group_lay.addLayout(attr_lay)
+        attr_info_lay = QHBoxLayout()
+        attr_info_lay.addWidget(name_line)
+        # attr_info_lay.addWidget(type_line)
+        attr_info_lay.addLayout(value_lay)
 
-    def create_value_widget(self, type_, value):
-        line_edit = QLineEdit()
-        if type_ == 'double':
-            line_edit.setValidator(QDoubleValidator())
-        line_edit.setText(str(value))
-        return line_edit
+        attr_children_lay = QVBoxLayout()
+
+        if children:
+            widget = self.create_group_widget('children of {0}'.format(name), children)
+            attr_children_lay.addWidget(widget)
+
+        attr_lay = QVBoxLayout()
+        attr_lay.addLayout(attr_info_lay)
+        attr_lay.addLayout(attr_children_lay)
+
+        return attr_lay
+
+    def create_value_layout(self, attr, lock=False):
+
+        lay = QHBoxLayout()
+        raw_value = attr.get()
+
+        if attr.type() == 'double3':
+            for value in raw_value:
+                line = QLineEdit()
+                line.setDisabled(lock)
+                line.setText(str(value))
+                lay.addWidget(line)
+        else:
+            line = QLineEdit()
+            line.setDisabled(lock)
+            line.setText(str(raw_value))
+            lay.addWidget(line)
+
+        return lay
